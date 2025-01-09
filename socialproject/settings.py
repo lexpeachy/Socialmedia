edf1c490ee1137ab15d66f4df60112124772f5db
evans kirimi <evanskirimi483@gmail.com>
2025-01-09 17:25:17 +0300

    commit

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SE')
+
+SECRET_KEY = 'django-insecure-z24#l5iq(l4drcdoqj+wlwllrjw7u-m662pdw)ry1sr2r39fa3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lexpeachy.pythonanywhere.com']
+ALLOWED_HOSTS = []#'lexpeachy.pythonanywhere.com'
 
 
 # Application definition
